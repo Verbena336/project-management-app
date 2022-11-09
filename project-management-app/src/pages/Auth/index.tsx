b4080af33ff3d39c83f5872089f7dcdbe68b6d95
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2022-11-09 20:42:27 +0300

    refactor: add intermediate folder renaming

--- project-management-app/src/pages/Auth/index.tsx
+++ project-management-app/src/pages/Auth/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import AuthWrap from './components/AuthWrap';
 import SignInForm from './components/SignInForm';
-import SignUpForm from './components/SignUpForm';
+import SignUpForm from './components/SignUp';
 
 const Auth = () => {
   return (
