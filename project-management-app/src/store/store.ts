18acdddb882292a692da019fc60bdac4ac135d84
Natalia Byzova <nata22011997@gmail.com>
2022-11-12 16:25:23 +0300

    feat: add tasks query

--- project-management-app/src/store/store.ts
+++ project-management-app/src/store/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import { authApi } from 'store/services/authApi';
+import { tasksApi } from './services/tasksApi';
 import authReducer from './reducers/authSlice';
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    [tasksApi.reducerPath]: tasksApi.reducer,
     auth: authReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware).concat(tasksApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
