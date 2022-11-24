846d34bc3139585b90b7909caa5c67eb84c92c7b
Natalia Byzova <nata22011997@gmail.com>
2022-11-24 21:15:28 +0300

    feat: add edit function

--- project-management-app/src/pages/Columns/components/Task/index.tsx
+++ project-management-app/src/pages/Columns/components/Task/index.tsx
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { Draggable } from 'react-beautiful-dnd';
+import { useTranslation } from 'react-i18next';
 
 import DeleteModal from 'components/Modals/DeleteModal';
 
-import { useDeleteTaskMutation } from 'store/services/boardsApi';
+import { useDeleteTaskMutation, useUpdateTaskMutation } from 'store/services/boardsApi';
 
 import styles from './index.module.scss';
 
 import { PropsTask } from './types';
+import CreateEditModal from 'components/Modals/CreateEditModal';
 
-const { text, task } = styles;
+const { text, task, textTitle, textDescr, controls } = styles;
 
-const Task = ({ task: { id, title, order, description }, index, boardId, columnId }: PropsTask) => {
+const Task = ({
+  task: { id, title, order, description, userId },
+  index,
+  boardId,
+  columnId,
+}: PropsTask) => {
   const [deleteTaskApi] = useDeleteTaskMutation();
-  const [isModal, setIsModal] = useState(false);
+  const [isModalDelete, setIsModalDelete] = useState(false);
+  const [isModalEdit, setIsModalEdit] = useState(false);
+  const [updateTask] = useUpdateTaskMutation();
+  const { t } = useTranslation();
 
   const deleteTask = async () => {
     try {
@@ -24,9 +34,58 @@ const Task = ({ task: { id, title, order, description }, index, boardId, columnI
     }
   };
 
+  // export type updateTaskRequest = {
+  //   boardId: string;
+  //   columnId: string;
+  //   taskId: string;
+  //   body: {
+  //     title: string;
+  //     order: number;
+  //     description: string;
+  //     userId: string;
+  //     boardId: string;
+  //     columnId: string;
+  //   };
+  // };
+
+  const handleTaskEdit = async (data: { title: string; description: string }) => {
+    const body = {
+      order,
+      userId,
+      boardId,
+      columnId,
+      title: data.title,
+      description: data.description,
+    };
+    const dataRequest = { boardId, columnId, taskId: id, body };
+    try {
+      const response = await updateTask(dataRequest).unwrap();
+      if (response.id) {
+        toast.success(t('toastContent.editBoard'));
+      } else {
+        throw new Error();
+      }
+    } catch {
+      toast.error(t('toastContent.serverError'));
+    }
+  };
+
+  const handleModal = () => setIsModalEdit(!isModalEdit);
+
   return (
     <>
-      {isModal && <DeleteModal handler={deleteTask} closeHandler={() => setIsModal(!isModal)} />}
+      {isModalEdit && (
+        <CreateEditModal
+          title={t('editTask.title')}
+          editValues={{ name: title, description }}
+          description={true}
+          handler={handleTaskEdit}
+          closeHandler={handleModal}
+        />
+      )}
+      {isModalDelete && (
+        <DeleteModal handler={deleteTask} closeHandler={() => setIsModalDelete(!isModalDelete)} />
+      )}
       <Draggable draggableId={id} index={index}>
         {(provided) => (
           <div
@@ -35,13 +94,17 @@ const Task = ({ task: { id, title, order, description }, index, boardId, columnI
             {...provided.draggableProps}
             {...provided.dragHandleProps}
           >
-            <div>
-              <p className={text}>
-                {order} {title}
-              </p>
-              <p className={text}>{description}</p>
+            <div className={text}>
+              <p className={textTitle}>{title}</p>
+              <p className={textDescr}>{description}</p>
             </div>
-            <button className="icon-task-action" onClick={() => setIsModal(!isModal)}></button>
+            <div className={controls}>
+              <button className="icon-task-edit" onClick={() => setIsModalEdit(!isModalEdit)} />
+              <button
+                className="icon-task-action"
+                onClick={() => setIsModalDelete(!isModalDelete)}
+              />
+            </div>
           </div>
         )}
       </Draggable>
